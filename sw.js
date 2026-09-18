6cfa81fda3622cf427cbc83185a5578bcfe8efd7
chadhoot78-coder <chadhoot78@gmail.com>
2026-09-18 14:10:11 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Duck Tracker service worker — bump CACHE when you change any file
-const CACHE = "duck-tracker-v4";
+const CACHE = "duck-tracker-v5";
 const FILES = ["./", "./index.html", "./manifest.json", "./icon.png"];
 
 self.addEventListener("install", e => {
